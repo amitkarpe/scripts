a45d504fdcc8403af9174c133225b11414428f45
Amit Karpe <amitkarpe@gmail.com>
2017-08-03 12:41:45 +0800

    Update cpu_load1.sh

--- cpu_load1.sh
+++ cpu_load1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Source @ https://stackoverflow.com/questions/2925606/how-to-create-a-cpu-spike-with-a-bash-command
+
 cat /proc/cpuinfo  | grep processor | wc -l
 
 fulload() {  dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null & }; fulload; read; killall dd
