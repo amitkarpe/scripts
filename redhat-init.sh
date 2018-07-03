a7b6991bfe5c0b917cb34fe2a72e4e2827df66f0
Amit Karpe <amitkarpe@gmail.com>
2018-07-03 11:47:13 +0800

    Create redhat-init.sh
    add package list to install
    add commands into bash profile

---
+++ redhat-init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# https://github.com/amitkarpe/scripts/invitations
+yum install vim tmux nano unzip wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct docker ansible php php-pear --assumeyes
+
+
+echo "echo ''" >> ~/.bash_profile
+echo "uptime" >> ~/.bash_profile
+echo "echo ''" >> ~/.bash_profile
+echo "free -h" >> ~/.bash_profile
+echo "echo ''" >> ~/.bash_profile
+echo "df -h /" >> ~/.bash_profile
+echo "echo ''" >> ~/.bash_profile
